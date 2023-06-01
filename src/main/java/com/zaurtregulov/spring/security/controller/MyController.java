7fc4a59a69c7c874463468c61146dcf66cde1e13
OlegChumin <tschumin.oleg@gmail.com>
2023-06-01 19:52:00 +0300

    Add new files

--- src/main/java/com/zaurtregulov/spring/security/controller/MyController.java
+++ src/main/java/com/zaurtregulov/spring/security/controller/MyController.java
@@ -2,11 +2,13 @@ package com.zaurtregulov.spring.security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class MyController {
 
-    @GetMapping("/")
+    @GetMapping("")
     public String getInfoAllEmps(){
         return "view-for-all-employees";
     }
